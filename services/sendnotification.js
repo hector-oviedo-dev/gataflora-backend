76063caa829d70278e175ec69dffec7a33b981a1
D0M41Nar <hector.ernesto.oviedo@gmail.com>
2018-05-06 23:34:22 -0300

    added example to MAIN, added to updatetoken

--- services/sendnotification.js
+++ services/sendnotification.js
@@ -8,10 +8,14 @@ router.post('/sendnotification', function(req, res, next) {
 	var uid = req.body.uid;
 	var token = req.body.token;
 	var msg = req.body.msg;
-	console.log("SENDING",req.body.uid);
-	res.json({"SENDING":uid});
 	
 	MAIN.sendMsg(uid, token, msg);
+	
+	var response = MAIN.getExample();
+	
+	res.json(response);
+	
+	console.log("sendnotification:", req.body.uid);
 });
 
 module.exports = router;
